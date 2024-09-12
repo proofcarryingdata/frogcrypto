9e45d5c1179d3205df7c2538b853821de2f7a6aa
Foret Fang <saurfang@users.noreply.github.com>
2024-09-12 15:41:00 -0700

    build

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,15 @@
 import { log } from "@repo/logger";
 import { createServer } from "./server";
 
+const isVercel = process.env.DEPLOYMENT_ENV === "vercel";
+
 const port = process.env.PORT || 5001;
 const server = createServer();
 
-server.listen(port, () => {
-  log(`api running on ${port}`);
-});
+if (!isVercel) {
+  server.listen(port, () => {
+    log(`api running on ${port}`);
+  });
+}
+
+export default server;
