3ec5bdb9e5428b6f5a9927c0d7bb175370eff27f
Foret Fang <saurfang@users.noreply.github.com>
2024-09-13 12:12:43 -0700

    copy contents

--- apps/pond/src/constants.ts
+++ apps/pond/src/constants.ts
@@ -4,7 +4,7 @@ export const FROM_SUBSCRIPTION_PARAM_KEY = "fromFrogSubscription";
 
 export const SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.frogcrypto.xyz"
+    ? "https://frogcrypto-api.vercel.app"
     : "http://localhost:4001";
 
 export const POD_TYPE_FROGCRYPTO_PLAYER_ID = "frogcrypto.playerId";
