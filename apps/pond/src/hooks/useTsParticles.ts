1cdf456f8802cdcc8cdbd3ad002e7b965532fe6c
Foret Fang <saurfang@users.noreply.github.com>
2024-10-16 15:35:58 -0700

    slim down tsparticle

--- apps/pond/src/hooks/useTsParticles.ts
+++ apps/pond/src/hooks/useTsParticles.ts
@@ -1,6 +1,12 @@
-import { useEffect, useState } from "react";
-import { loadFull } from "tsparticles";
 import { tsParticles } from "@tsparticles/engine";
+import { loadExternalTrailInteraction } from "@tsparticles/interaction-external-trail";
+import { loadEmittersPlugin } from "@tsparticles/plugin-emitters";
+import { loadTextShape } from "@tsparticles/shape-text";
+import { loadSlim } from "@tsparticles/slim";
+import { loadDestroyUpdater } from "@tsparticles/updater-destroy";
+import { loadRollUpdater } from "@tsparticles/updater-roll";
+import { loadWobbleUpdater } from "@tsparticles/updater-wobble";
+import { useEffect, useState } from "react";
 
 let loadPromise: Promise<void> | undefined;
 
@@ -13,7 +19,13 @@ function useTsParticles(): boolean {
     }
 
     const load = async (): Promise<void> => {
-      await loadFull(tsParticles);
+      await loadSlim(tsParticles);
+      await loadExternalTrailInteraction(tsParticles);
+      await loadEmittersPlugin(tsParticles);
+      await loadTextShape(tsParticles);
+      await loadDestroyUpdater(tsParticles);
+      await loadRollUpdater(tsParticles);
+      await loadWobbleUpdater(tsParticles);
       setReady(true);
     };
 
