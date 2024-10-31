2a36356823e0263eef15f8192c1e05d29ca141c4
Foret Fang <saurfang@users.noreply.github.com>
2024-10-31 09:44:45 -0700

    fix button

--- apps/pond/src/ZappButton.tsx
+++ apps/pond/src/ZappButton.tsx
@@ -1,17 +1,8 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Provider } from "jotai";
+import { QueryClient } from "@tanstack/react-query";
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { FrogEmoji } from "./components/Frog";
-import useInitializeUser from "./hooks/useInitializeUser";
-import {
-  ParcnetIframeProvider,
-  useParcnetClientConnected,
-} from "./hooks/useParcnetClient";
-import { useUserState } from "./hooks/useUserState";
 import "./index.css";
-import { hasToken, trpc, trpcClient } from "./trpc";
 
 const queryClient = new QueryClient();
 
@@ -41,54 +32,12 @@ function StaticButton() {
   );
 }
 
-function DynamicButton() {
-  const { data: userState } = useUserState();
-  if (!userState) return <StaticButton />;
-
-  return (
-    <FrogButton>
-      <h1 className="text-frog-score text-moss-700 font-mono self-center hover:text-white active:text-white">
-        Score: {userState.myScore.score}
-      </h1>
-      <FrogEmoji />
-    </FrogButton>
-  );
-}
-
 function App() {
-  const isConnected = useParcnetClientConnected();
-  const { hasIdentity, error } = useInitializeUser();
-  const isReady = isConnected && hasIdentity;
-
-  if (hasToken()) {
-    return <DynamicButton />;
-  }
-
-  if (!isReady) {
-    return <StaticButton />;
-  }
-
-  if (error) {
-    return <StaticButton />;
-  }
-
-  return <DynamicButton />;
+  return <StaticButton />;
 }
 
 createRoot(root).render(
   <StrictMode>
-    <Provider>
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>
-          <ParcnetIframeProvider skipConnection>
-            <App />
-
-            {window.self === window.top && (
-              <ReactQueryDevtools initialIsOpen={false} />
-            )}
-          </ParcnetIframeProvider>
-        </QueryClientProvider>
-      </trpc.Provider>
-    </Provider>
+    <App />
   </StrictMode>
 );
