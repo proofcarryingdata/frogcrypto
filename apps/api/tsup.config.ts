43e11dfc9c6be43198edfe45d069a053b37aae30
Foret Fang <saurfang@users.noreply.github.com>
2024-11-09 22:58:00 +0700

    fix build

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -4,7 +4,6 @@ export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts"],
   clean: true,
   format: ["cjs"],
-  onSuccess:
-    "cp -a node_modules/@pcd/proto-pod-gpc-artifacts/*.{zkey,json} dist && cp -a views dist",
+  onSuccess: "cp -a views dist",
   ...options,
 }));
